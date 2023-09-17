9f0a94793dcf512178ce5d997aeb7bd99a655998
Alex C. Schaefer <alexcschaefer@me.com>
2023-09-16 18:49:17 -0700

    Add files via upload

---
+++ Blossom.py
@@ -0,0 +1,97 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit
+from PyQt5.QtCore import QTimer
+import requests
+import json
+import sys
+import subprocess
+
+class AppSettings:
+    def __init__(self):
+        self.filename = "settings.json"
+        self.load()
+
+    def load(self):
+        try:
+            with open(self.filename, 'r') as f:
+                self.data = json.load(f)
+        except FileNotFoundError:
+            self.data = {
+                'youtube_api_key': "",
+                'stream_schedule': {'day': 'Saturday', 'start': '9:00', 'end': '9:45'},
+                'announcements_url': ""
+            }
+            self.save()
+
+    def save(self):
+        with open(self.filename, 'w') as f:
+            json.dump(self.data, f, indent=4)
+
+def execute_command(command):
+    try:
+        subprocess.run(command, shell=True, check=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed: {e}")
+        return False
+
+class AppWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.settings = AppSettings()
+
+        # Read the current version from a file
+        with open("version.txt", "r") as f:
+            self.current_version = f.read().strip()
+
+        # Setup a timer to check for updates every hour
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.check_for_updates)
+        self.timer.start(60 * 60 * 1000)  # 1 hour in milliseconds
+
+        self.initUI()
+
+    def initUI(self):
+        # Initialize UI elements here
+        self.setGeometry(100, 100, 640, 480)
+        self.setWindowTitle('Blossom and Bloom')
+
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setGeometry(50, 50, 400, 300)
+
+        self.start_button = QPushButton("Start Stream", self)
+        self.start_button.move(50, 400)
+        self.start_button.clicked.connect(self.start_stop_stream)
+
+    def start_stop_stream(self):
+        # Implement the logic to start/stop stream
+        pass
+
+    def check_for_updates(self):
+        try:
+            # Fetch the latest version from GitHub
+            response = requests.get("https://raw.githubusercontent.com/yourusername/yourrepo/main/version.txt")
+            latest_version = response.text.strip()
+
+            if self.current_version != latest_version:
+                self.update_app(latest_version)
+        except requests.RequestException:
+            print("Could not check for updates.")
+
+    def update_app(self, new_version):
+        print(f"Updating app to version {new_version}")
+
+        if not execute_command("git pull origin main"):
+            print("Failed to update code from repository.")
+            return
+
+        if execute_command("python3 Blossom.py"):
+            print("Successfully updated and restarted the app.")
+            self.close()
+        else:
+            print("Failed to restart the app.")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = AppWindow()
+    ex.show()
+    sys.exit(app.exec_())
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if running as superuser
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+# Install required packages
+apt update && apt install -y python3 python3-pip git
+pip3 install PyQt5 requests
+
+# Clone your repo (replace with your actual repo URL)
+git clone https://github.com/yourusername/yourrepo.git /path/to/app/folder
+
+# Add a desktop shortcut
+echo "[Desktop Entry]
+Version=1.0
+Name=Blossom and Bloom
+Exec=python3 /path/to/app/folder/Blossom.py
+Icon=/path/to/app/folder/icon.png
+Terminal=false
+Type=Application
+" > /home/pi/Desktop/Blossom.desktop
+
+# Make it executable
+chmod +x /home/pi/Desktop/Blossom.desktop
+
+# Enable auto-start (create an autostart folder if it doesn't exist)
+mkdir -p /home/pi/.config/autostart
+cp /home/pi/Desktop/Blossom.desktop /home/pi/.config/autostart/
+
+echo "Installation complete."
