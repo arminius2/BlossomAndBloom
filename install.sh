de247e6acb752867858e460146ce638f62e94327
Alex Schaefer <alex_schaefer@apple.com>
2023-09-16 19:06:17 -0700

    update install script

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ apt update && apt install -y python3 python3-pip git
 pip3 install PyQt5 requests
 
 # Clone your repo (replace with your actual repo URL)
-git clone https://github.com/arminius2/BlossomAndBloom.git /path/to/app/folder
+git clone https://github.com/arminius2/BlossomAndBloom.git /home/pi/BlossomApp
 
 # Add a desktop shortcut
 echo "[Desktop Entry]
 Version=1.0
 Name=Blossom and Bloom
-Exec=python3 /path/to/app/folder/Blossom.py
-Icon=/path/to/app/folder/icon.png
+Exec=python3 /home/pi/BlossomApp/Blossom.py
+Icon=/home/pi/BlossomApp/icon.png
 Terminal=false
 Type=Application
 " > /home/pi/Desktop/Blossom.desktop
