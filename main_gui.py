bfde5cf5088827f02757c10569c7852cd29d0af0
Alex Schaefer <alex_schaefer@apple.com>
2023-10-15 14:47:13 -0700

    fffff

--- main_gui.py
+++ main_gui.py
@@ -5,53 +5,43 @@ from WebKit import *
 import youtube_stream
 import objc
 
-# Create a delegate protocol for the MainWindowController
-MainWindowControllerDelegate = objc.informal_protocol(
-    "MainWindowControllerDelegate",
-    [
-        objc.selector(None, selector="startStream", signature="v@:"),
-        objc.selector(None, selector="stopStream", signature="v@:"),
-    ]
-)
-
 class MyWKNavigationDelegate(NSObject):
     def webView_decidePolicyForNavigationAction_decisionHandler_(self, webview, navigationAction, decisionHandler):
-        print("Navigation action:", navigationAction)
         decisionHandler(WKNavigationActionPolicyAllow)
 
 class MainWindowController(NSWindowController):
-    delegate = objc.ivar(type=objc_id)
+    delegate = objc.ivar()
 
     def loadWindow(self):
         screenFrame = NSScreen.mainScreen().frame()
-        self._window = NSWindow.alloc().initWithContentRect_styleMask_backing_defer_(
+        self.window = NSWindow.alloc().initWithContentRect_styleMask_backing_defer_(
             screenFrame,
             NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable,
             NSBackingStoreBuffered,
             False
         )
-        self.setWindow_(self._window)
+        self.setWindow_(self.window)
 
     def windowDidLoad(self):
-        screenFrame = self.window().frame()
+        screenFrame = self.window.frame()
 
         self.webView = WKWebView.alloc().initWithFrame_(screenFrame)
         self.webView.setNavigationDelegate_(MyWKNavigationDelegate.alloc().init())
         request = NSURLRequest.requestWithURL_(NSURL.URLWithString_("https://www.canva.com/"))
         self.webView.loadRequest_(request)
-        self.window().contentView().addSubview_(self.webView)
+        self.window.contentView().addSubview_(self.webView)
 
         self.startStopButton = NSButton.alloc().initWithFrame_(NSMakeRect(20, 20, 100, 40))
         self.startStopButton.setTitle_("Start")
         self.startStopButton.setTarget_(self)
         self.startStopButton.setAction_("toggleStream:")
-        self.window().contentView().addSubview_(self.startStopButton)
+        self.window.contentView().addSubview_(self.startStopButton)
 
         self.statusTextField = NSTextField.alloc().initWithFrame_(NSMakeRect(0, 0, screenFrame.size.width, 20))
         self.statusTextField.setAlignment_(NSCenterTextAlignment)
         self.statusTextField.setBackgroundColor_(NSColor.blackColor())
         self.statusTextField.setTextColor_(NSColor.whiteColor())
-        self.window().contentView().addSubview_(self.statusTextField)
+        self.window.contentView().addSubview_(self.statusTextField)
 
     def toggleStream_(self, sender):
         if self.startStopButton.title() == "Start":
