374aadc5b3078e46b9cbac1cb2735fcb1d67b137
Alex Schaefer <alex_schaefer@apple.com>
2023-10-15 14:08:22 -0700

    fixing webkit

--- main_gui.py
+++ main_gui.py
@@ -26,7 +26,6 @@ class AppDelegate(NSObject):
         self.window.setLevel_(20)  # Numeric constant for NSMainMenuWindowLevel
 
         web_config = WKWebViewConfiguration.alloc().init()
-        web_config.preferences.setValue_forKey_(True, "developerExtrasEnabled")
         self.webView = WKWebView.alloc().initWithFrame_configuration_(screenFrame, web_config)
         self.webView.setCustomUserAgent_("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15")
 
