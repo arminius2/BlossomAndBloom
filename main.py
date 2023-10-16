9141926788c1729c34ba82991ac48c9732601023
Alex Schaefer <alex_schaefer@apple.com>
2023-10-15 18:15:24 -0700

    asdfasdf

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ import requests
 import keyring
 import time
 import signal
+import fcntl
 import daemon
 from util.version_check import check_version
 from zeroconf import ServiceInfo, Zeroconf
 from httpserver import run_http_server
 
+running = True
+
 def handle_exit(signum, frame):
+    global running
     print("Received termination signal. Exiting...")
+    running = False
     os._exit(0)
 
 def check_stream_key():
@@ -50,8 +55,24 @@ def setup_zeroconf(PORT):
     zeroconf.register_service(wsInfo)
 
 def main_program():
-    signal.signal(signal.SIGTERM, handle_exit)
-    signal.signal(signal.SIGINT, handle_exit)
+    lock_file_path = 'program.lock'
+    
+    # Try to acquire the lock
+    try:
+        lock_file = open(lock_file_path, 'w')
+        fcntl.lockf(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError:
+        print("Another instance of the program is already running. Exiting...")
+        return
+
+    # Register the lock release for program termination
+    def release_lock():
+        fcntl.lockf(lock_file, fcntl.LOCK_UN)
+        lock_file.close()
+        os.remove(lock_file_path)
+    
+    signal.signal(signal.SIGTERM, lambda signum, frame: (handle_exit(signum, frame), release_lock()))
+    signal.signal(signal.SIGINT, lambda signum, frame: (handle_exit(signum, frame), release_lock()))
 
     check_internet_connection()
     check_version()
@@ -63,8 +84,10 @@ def main_program():
     http_server_thread = threading.Thread(target=run_http_server,args=(PORT,))
     http_server_thread.start()
 
-    while True:
-        time.sleep(100)
+    while running:
+        time.sleep(10)
+
+    release_lock()         
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run the program.')
