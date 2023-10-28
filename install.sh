323c72210a47bf633e82817f61db55fa7d2814d2
Alex Schaefer <alex_schaefer@apple.com>
2023-10-28 07:37:54 -0700

    add api steering of the stream url

--- install.sh
+++ install.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+WIPE_CREDENTIALS=false
+
+# Check for the --wipe flag
+for arg in "$@"; do
+  if [ "$arg" == "--wipe" ]; then
+    WIPE_CREDENTIALS=true
+    break
+  fi
+done
+
 # Check if the script is run as root
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
   exit
+fi
+
+if [ "$WIPE_CREDENTIALS" = true ]; then
+  echo "Wiping existing credentials..."
+  keyring delete BlossomAndBloom YouTubeStream
+  keyring delete BlossomAndBloom YouTubeAPIKey
+  keyring delete BlossomAndBloom YouTubeClientID
+  keyring delete BlossomAndBloom YouTubeClientSecret
+  echo "Credentials wiped."
+  exit 0
 fi
 
 # Killing all instances of the script
@@ -76,6 +96,31 @@ if [ -z "$youtube_key" ]; then
 
   python3 -c "import keyring; keyring.set_password('BlossomAndBloom', 'YouTubeStream', '$stream_key')"
 fi
+
+# Check for API key
+api_key=$(keyring get BlossomAndBloom YouTubeAPIKey)
+if [ -z "$api_key" ]; then
+  echo "No YouTube API key is set."
+  read -p "Enter your YouTube API key: " api_key
+  keyring set BlossomAndBloom YouTubeAPIKey "$api_key"
+fi
+
+# Check for OAuth 2.0 Client ID
+oauth_client_id=$(keyring get BlossomAndBloom YouTubeClientID)
+if [ -z "$oauth_client_id" ]; then
+  echo "No YouTube OAuth 2.0 Client ID is set."
+  read -p "Enter your YouTube OAuth 2.0 Client ID: " oauth_client_id
+  keyring set BlossomAndBloom YouTubeClientID "$oauth_client_id"
+fi
+
+# Check for OAuth 2.0 Client Secret
+oauth_client_secret=$(keyring get BlossomAndBloom YouTubeClientSecret)
+if [ -z "$oauth_client_secret" ]; then
+  echo "No YouTube OAuth 2.0 Client Secret is set."
+  read -p "Enter your YouTube OAuth 2.0 Client Secret: " oauth_client_secret
+  keyring set BlossomAndBloom YouTubeClientSecret "$oauth_client_secret"
+fi
+
 
 # Change ownership of the entire installation to the non-root user
 chown -R $(logname):$(id -gn $(logname)) $HOME/.blossomandbloom
--- youtube_stream.py
+++ youtube_stream.py
@@ -1,5 +1,8 @@
 import subprocess
 import keyring
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 def start_stream():
     stream_key = keyring.get_password("BlossomAndBloom", "YouTubeStream")
@@ -28,3 +31,40 @@ def start_stream():
         with subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1, universal_newlines=True) as proc:
             for line in iter(proc.stdout.readline, ''):
                 print(line)  # or
+
+def get_youtube_client():
+    api_key = keyring.get_password("BlossomAndBloom", "YouTubeAPIKey")
+    oauth_client_id = keyring.get_password("BlossomAndBloom", "YouTubeClientID")
+    oauth_client_secret = keyring.get_password("BlossomAndBloom", "YouTubeClientSecret")
+
+    # Your OAuth 2.0 Client ID JSON file path
+    client_secrets_file = "path/to/your/client_secret.json"
+
+    # Check if credentials exist
+    credentials = None
+    if os.path.exists("credentials.json"):
+        credentials = Credentials.from_authorized_user_file("credentials.json", ["https://www.googleapis.com/auth/youtube.force-ssl"])
+    
+    # If there are no (valid) credentials, prompt the user to log in
+    if not credentials or not credentials.valid:
+        flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, ["https://www.googleapis.com/auth/youtube.force-ssl"])
+        credentials = flow.run_console()
+        with open("credentials.json", "w") as f:
+            f.write(credentials.to_json())
+
+    youtube = build("youtube", "v3", credentials=credentials)
+
+    return youtube
+
+def get_streaming_url(youtube):
+    request = youtube.liveBroadcasts().list(
+        broadcastStatus="active",
+        part="snippet,contentDetails"
+    )
+    response = request.execute()
+    
+    if "items" in response:
+        for item in response["items"]:
+            print("Streaming URL:", item["contentDetails"]["monitorStream"]["embedHtml"])
+    else:
+        print("No active streams found.")    
